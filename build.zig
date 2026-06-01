cfcdea2b9036880eee995e45396adfad936e5c73
Meghan Denny <hello@nektro.net>
2026-06-01 16:11:13 -0700

    update to zig 0.15.2

--- build.zig
+++ build.zig
@@ -2,15 +2,17 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const mode = b.option(std.builtin.OptimizeMode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     const mod = b.addModule("cookies", .{ .root_source_file = b.path("cookies.zig") });
 
     const tests = b.addTest(.{
-        .root_source_file = b.path("test.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("test.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     tests.root_module.addImport("cookies", mod);
     tests.root_module.link_libc = true;
